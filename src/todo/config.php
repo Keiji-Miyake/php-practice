ce9d5e1436c4be9aa9b6b90f5ad0b94fdbd0c08c
Keiji Miyake <k.miyake+dev@gmail.com>
2020-11-23 11:28:39 +0900

    修正

--- src/todo/config.php
+++ src/todo/config.php
@@ -2,6 +2,6 @@
 
 ini_set('display_errors', 1);
 
-define('DSN', 'mysql:host=localhost;dbname=dotinstall_todo_app');
-define('DB_USERNAME', 'dbuser');
-define('DB_PASSWORD', '$6Ydnt7p');
+define('DSN', 'mysql:host=db;dbname=todo_app');
+define('DB_USERNAME', 'dev');
+define('DB_PASSWORD', 'pass');
